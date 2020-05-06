3e480d55e8a6a0909bd44aa90ec22a8e76263043
Fatunbi David Oluwakayode <fatunbidavidkayode@gmail.com>
2020-05-06 04:36:31 -0700

    shuffle enabled

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockboard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if(lockboard) return;
+    if(this === firstCard) return;
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -16,18 +19,43 @@ function flipCard() {
         secondCard = this;
 
         //do cards match?
-        if (firstCard.dataset.framework ===
-            secondCard.dataset.framework) {
-            //it's a match
-            firstCard.removeEventListner('click', flipCard);
-            secondCard.removeEventListner('click', flipCard);
-        } else {
-            //not a match
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1500);
-        }
+       checkForMatch();
+           
+       
     }
 }
+function checkForMatch(){
+    if (firstCard.dataset.framework ===
+    secondCard.dataset.framework) {
+        //it's a match
+      disableCards();
+    } else {
+        //not a match
+        unflipcards();
+    }
+}
+function disableCards(){
+    firstCard.removeEventListner('click', flipCard);
+    secondCard.removeEventListner('click', flipCard);
+    resetBoard();
+}
+function unflipcards(){
+    lockboard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1500);
+    
+}
+function resetBoard(){
+    [hasFlippedCard, lockboard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+(function shuffle() {
+    cards.forEach(card =>{
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    })
+})();
 cards.forEach(card => card.addEventListener('click', flipCard));
